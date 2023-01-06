b320233ef6788caeaf69610b86c7eb35aa90490f
hackerncoder <hackerncoder@debian>
2023-01-06 11:39:28 +0100

    Do gui exercises 1-3

--- S2010_gui_exercise1.py
+++ S2010_gui_exercise1.py
@@ -10,4 +10,26 @@ When your program is complete, push it to your github repository.
 Then send this Teams message to your teacher: <filename> done
 Thereafter go on with the next file.
 """
+import tkinter as tk
 
+main_window = tk.Tk()
+main_window.title("my first GUI")
+main_window.geometry("110x150")
+
+padx = 8
+pady = 4
+
+frame_1 = tk.LabelFrame(main_window, text="Container")
+frame_1.grid(row=0, column=0, padx=padx, pady=pady, sticky=tk.N)
+
+button_1 = tk.Button(frame_1, text="Create")
+button_1.grid(row=2, column=1, padx=padx, pady=pady)
+
+label_1 = tk.Label(frame_1, text="Id")
+label_1.grid(row=0, column=1, padx=padx, pady=pady)
+
+entry_1 = tk.Entry(frame_1, width=4, justify="center")
+entry_1.grid(row=1, column=1, padx=padx, pady=padx)
+
+if __name__ == "__main__":
+    main_window.mainloop()
